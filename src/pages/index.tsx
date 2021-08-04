5928ba9256cbf87b0f0145de492757aba924a97c
ET <casualorient@gmail.com>
2021-08-04 23:38:10 +0800

    temporary useEffect on index to /login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,15 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { NextPage } from 'next'
 
-const Index = () => {
+const Index: NextPage = () => {
 	const router = useRouter()
 	useEffect(() => {
 		router.push('/login')
 	})
+
+	return <div></div>
 }
 
 export default Index
