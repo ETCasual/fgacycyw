d02aea17c20a52f6ba3d3aa3b58055b3e7007ffc
ETCasual <casualorient@gmail.com>
2021-08-15 17:18:32 +0800

    timeline init

---
+++ src/components/Timeline.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {
+	VerticalTimeline,
+	VerticalTimelineElement
+} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+
+export const Timeline: React.FC = () => {
+	return <VerticalTimeline></VerticalTimeline>
+}
