6ba755045e9a0d73c5cd94e83efe04529a1d780a
ETCasual <casualorient@gmail.com>
2021-08-19 06:18:33 +0800

    Words from organizers

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@ const withPWA = require('next-pwa')
 
 module.exports = withPWA({
 	pwa: {
-		dest: 'public'
-		// swSrc: './service-worker.js',
-		// register: true,
-		// dynamicStartUrl: true,
-		// dynamicStartUrlRedirect: '/login'
+		dest: 'public',
+		swSrc: './service-worker.js',
+		register: true,
+		dynamicStartUrl: true,
+		dynamicStartUrlRedirect: '/login'
 	},
 	webpack5: true
 })
